cc41e30649705d902e16d51ec402ba266e19a695
awayer_mini <xdawayer@gmail.com>
2026-06-06 05:40:44 +0800

    feat(articles): publish journal-prompts (claude v8) [autopilot]

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -140,6 +140,7 @@ import { fullMoonJune2026En } from "./full-moon-june-2026";
 import { moonJournalEn } from "./moon-journal";
 import { fullMoonJournalPromptsEn } from "./full-moon-journal-prompts";
 import { newMoonJournalPromptsEn } from "./new-moon-journal-prompts";
+import { journalPromptsEn } from "./journal-prompts";
 // All articles organized by language
 
 import {
@@ -327,6 +328,7 @@ const ARTICLES_EN: WikiArticle[] = [
   moonJournalEn,
   fullMoonJournalPromptsEn,
   newMoonJournalPromptsEn,
+  journalPromptsEn,
 ];
 
 const ARTICLES_ZH: WikiArticle[] = [
