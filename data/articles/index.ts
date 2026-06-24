cc5bc00ee70fd5d1059a202fe494a59ab5eb5d8d
awayer_mini <xdawayer@gmail.com>
2026-06-24 20:21:52 +0800

    feat(articles): publish greta-lee-zodiac-sign (claude v8) [autopilot]

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -269,6 +269,7 @@ import { brunoFernandesZodiacSignZh } from "./bruno-fernandes-zodiac-sign";
 import { rhaenyraTargaryenZodiacSignZh } from "./rhaenyra-targaryen-zodiac-sign";
 import { cancerSeason2026AstrologyZh } from "./cancer-season-2026-astrology";
 import { kylieJennerZodiacSignEn } from "./kylie-jenner-zodiac-sign";
+import { gretaLeeZodiacSignEn } from "./greta-lee-zodiac-sign";
 // All articles organized by language
 
 import {
@@ -529,6 +530,7 @@ const ARTICLES_EN: WikiArticle[] = [
   cancerSeason2026AstrologyEn,
   rhaenyraTargaryenZodiacSignEn,
   kylieJennerZodiacSignEn,
+  gretaLeeZodiacSignEn,
 ];
 
 const ARTICLES_ZH: WikiArticle[] = [
