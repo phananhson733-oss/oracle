6540a2b2066bcea49d02a2f4a5b5342621c83805
awayer_mini <xdawayer@gmail.com>
2026-07-01 22:49:39 +0800

    feat(articles): publish bella-hadid-birth-chart (claude v8) [autopilot]

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -295,6 +295,7 @@ import { cardiBBirthChartEn } from "./cardi-b-birth-chart";
 import { prideMonthAstrologyEn } from "./pride-month-astrology";
 import { totalSolarEclipse2026En } from "./total-solar-eclipse-2026";
 import { mayaJointBirthChartEn } from "./maya-joint-birth-chart";
+import { bellaHadidBirthChartEn } from "./bella-hadid-birth-chart";
 // All articles organized by language
 
 import {
@@ -581,6 +582,7 @@ const ARTICLES_EN: WikiArticle[] = [
   prideMonthAstrologyEn,
   totalSolarEclipse2026En,
   mayaJointBirthChartEn,
+  bellaHadidBirthChartEn,
 ];
 
 const ARTICLES_ZH: WikiArticle[] = [
