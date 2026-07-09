464a0c68fd33f8bf7b645f18d13ccd963e1be416
awayer_mini <xdawayer@gmail.com>
2026-07-10 00:08:31 +0800

    feat(articles): publish zendaya-birth-chart (claude v8) [autopilot]

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -330,6 +330,7 @@ import { moSalahZodiacSignEn } from "./mo-salah-zodiac-sign";
 import { justinVerlanderBirthChartEn } from "./justin-verlander-birth-chart";
 import { jackAntonoffBirthChartEn } from "./jack-antonoff-birth-chart";
 import { ayoEdebiriBirthChartEn } from "./ayo-edebiri-birth-chart";
+import { zendayaBirthChartEn } from "./zendaya-birth-chart";
 // All articles organized by language
 
 import {
@@ -651,6 +652,7 @@ const ARTICLES_EN: WikiArticle[] = [
   justinVerlanderBirthChartEn,
   jackAntonoffBirthChartEn,
   ayoEdebiriBirthChartEn,
+  zendayaBirthChartEn,
 ];
 
 const ARTICLES_ZH: WikiArticle[] = [
