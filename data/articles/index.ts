ab1c009ad020cecef4424c8b27491ab468374c1d
awayer_mini <xdawayer@gmail.com>
2026-07-15 21:52:32 +0800

    feat(articles): publish cody-bellinger-birth-chart (claude v8) [autopilot]

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -356,6 +356,7 @@ import { whyDoIFeelStuckInMyCareerEn } from "./why-do-i-feel-stuck-in-my-career"
 import { saturnReturnGuideEn } from "./saturn-return-guide";
 import { saturnReturnInScorpioEn } from "./saturn-return-in-scorpio";
 import { secondSaturnReturnEn } from "./second-saturn-return";
+import { codyBellingerBirthChartEn } from "./cody-bellinger-birth-chart";
 // All articles organized by language
 
 import {
@@ -703,6 +704,7 @@ const ARTICLES_EN: WikiArticle[] = [
   saturnReturnGuideEn,
   saturnReturnInScorpioEn,
   secondSaturnReturnEn,
+  codyBellingerBirthChartEn,
 ];
 
 const ARTICLES_ZH: WikiArticle[] = [
